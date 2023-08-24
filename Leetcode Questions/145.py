65caccf14c53632daa38cd71de9fc6267e50bd20
yashsharma127 <yashshar.2323@gmail.com>
2023-08-24 23:04:50 +0530

    adding question 145

---
+++ Leetcode Questions/145.py
@@ -0,0 +1,17 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        if root is None:
+            return []
+        return (self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val])
+
+root = [1,None,2,3]
+solution = Solution()
+result = solution.preorderTraversal(root)
+
+print(result)
